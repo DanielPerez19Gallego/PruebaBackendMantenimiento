e2cb66f25891ac6126b026bfb6517fb7f0e7cf91
DanielPerez19Gallego <91479399+DanielPerez19Gallego@users.noreply.github.com>
2022-12-08 21:30:25 +0100

    Update RestaurantController.java

--- src/main/java/uclm/esi/equipo01/http/RestaurantController.java
+++ src/main/java/uclm/esi/equipo01/http/RestaurantController.java
@@ -25,7 +25,7 @@ import uclm.esi.equipo01.service.RestaurantService;
 *
 **********************************************************************/
 
-@CrossOrigin(origins = {"https://ticomo01.web.app", "http://localhost:3000"})
+@CrossOrigin(origins = {"https://ticomo-c4ec0.web.app/", "http://localhost:3000"})
 @RestController
 @RequestMapping("restaurant")
 public class RestaurantController {
